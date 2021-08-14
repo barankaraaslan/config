1cdab91aebee91c345de44c7beb1cadf265172b5
Baran Karaaslan <barankaraaslan_06@hotmail.com>
2021-08-14 03:20:39 +0300

    improve test and use full path when adding stat.ini

--- test.sh
+++ test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-
-sudo mkdir -p -m 777 /var/config
-
-rm -rf ~/dummy_package
-rm -rf /var/config/dummy_package
-./config dummy_package --create
-
 rm -rf test/
-./config dummy_package --inside clone . `pwd`/test/
+mkdir -p test/var/config
+mkdir -p test/home/randusername
+
+rm -rf test/home/randusername/dummy_package
+rm -rf test/var/config/dummy_package
+./config dummy_package --work_tree=test/ --config_home=test/home/randusername --stat_home=test/var/config --create 
+
+rm -rf test/cloned
+./config dummy_package --work_tree=test/ --config_home=test/home/randusername --stat_home=test/var/config --inside clone . `pwd`/test/cloned
